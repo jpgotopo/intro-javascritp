4b28e429e25516379c0871280275cce45ef09e13
jpgotopo <jpgotopo@gmail.com>
2020-12-26 20:54:00 -0500

    Terminando lección Arreglos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-const persona = {
-    nombre: 'Tony',
-    apellido: 'Stark',
-    edad: 45,
-    direccion: {
-        ciudad: 'New York',
-        zip: 45683224,
-        lat: 14.345621,
-        lng: 34.587942
-    }
-}
-const persona2 = { ...persona}
-persona2.nombre = 'Peter'
-console.log(persona);
-console.log(persona2);
+console.log('object');
