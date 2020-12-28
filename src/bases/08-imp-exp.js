446315c2856e4af55f5780a24e521d1bb6df2d4e
jpgotopo <jpgotopo@gmail.com>
2020-12-28 07:41:19 -0500

    Importaciones y exportaciones

---
+++ src/bases/08-imp-exp.js
@@ -0,0 +1,26 @@
+// import { heroes } from './data/heroes';
+import heroes, { owners } from '../data/heroes';
+// const getHeroeById = (id) => {
+//     return heroes.find((heroe) => {
+//         if (heroe.id === id) {
+//             return true;
+//         }else {
+//             return false;
+//         }
+//     });
+// }
+
+// const getHeroeById = (id) => {
+//     return heroes.find((heroe) => heroe.id === id );  //heroe dentro del paréntesis es el argumento de la función que recibe el find
+// }
+console.log(owners);
+
+const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id );  //heroe dentro del paréntesis es el argumento de la función que recibe el find
+
+
+const resultado = getHeroeById(2);
+console.log(resultado);
+
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner)
+
+console.log(getHeroesByOwner('DC'))
