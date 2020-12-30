ee33980df3da54d28ec738f2c48076f484877d7d
Jpgotopo <jpgotopo@GMAIL.COM>
2020-12-30 01:15:56 -0500

    Fetch en JS

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,18 @@
+
+const apiKey = 'YiBg7GYAuEyhz7YKrSU5faDDC4gC3FXD';
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+peticion
+    .then( resp => resp.json())
+    .then(({data}) =>{
+        const { url } = data.images.original;
+        
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+
+    })
+    .catch( console.warn );
